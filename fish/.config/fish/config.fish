e7897981f9c68cfdc2944a20a8109e3c2f2edfc4
Peter Kerins <pete.kerins@gmail.com>
2026-01-02 14:56:55 +0000

    Update fish configuration: set editor and customize color settings

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,3 +4,11 @@ if status is-interactive
     bind \ed kill-word
 end
 
+set -gx EDITOR "nvim"
+
+set fish_color_cwd 7aa2f7
+set fish_color_command 7aa2f7
+set fish_color_error f7768e
+set fish_color_valid_path 9ece6a
+set fish_color_warning e0af68
+set fish_color_autosuggestion bb9af7 --dim
