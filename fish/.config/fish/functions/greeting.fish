2d1b5246784708ec0940745322f6be47ab217a2d
Peter Kerins <pete.kerins@gmail.com>
2026-01-02 14:56:19 +0000

    Update greeting function: add random emoji selection to output

--- fish/.config/fish/functions/greeting.fish
+++ fish/.config/fish/functions/greeting.fish
@@ -3,8 +3,10 @@ function greeting
                    "Fuck em up" \
                    "Don't do it." 
 
+    set -l emojis "🐟" "👻"
     set -l num_phrases (count $phrases)
     set -l random_index (random 1 $num_phrases)
+    set -l random_emoji_index (random 1 2)
 
-    echo $phrases[$random_index] 🐟
+    echo $phrases[$random_index] $emojis[$random_emoji_index]
 end
